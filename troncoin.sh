18bdbde13c5e96c55de06ad1dd1f798f43296975
minner006 <92844456+minner006@users.noreply.github.com>
2021-10-20 02:14:08 -0400

    Create troncoin.sh

---
+++ troncoin.sh
@@ -0,0 +1,6 @@
+# troncoin
+sudo apt update
+! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
+! tar -xf lolMiner_v1.28a_Lin64.tar.gz
+%cd 1.28a/
+!./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TF7VDMzhciHkfyyaeMRnoLeuPH9T9k6okz.colab --ethstratum ETHPROXY
